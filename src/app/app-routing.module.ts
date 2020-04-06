518e038ab7e417342967f075803e1de8b60e2a61
Caelin Sutch <caelinsutch@gmail.com>
2020-04-05 18:59:10 -0700

    Added customer module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,9 @@ const routes: Routes = [
   },
   {
     path: 'kanban', loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule ), canActivate: [AuthGuard],
+  },
+  {
+    path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule ),
   }
 ];
 
